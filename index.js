576b109866c099813b1291e97662d71e23eb0d55
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 20:20:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+class Driver {
+  constructor(startDate, yearsExperienceFromBeginningOf){
+    this.date = date;
+    this.yearsExperienceFromBeginningOf =  function(){}
+  }
+}
+
+
+class Route {
+  constructor (blocksTravelled, endingLocation)
+  {
+    this.blocksTravelled = blocksTravelled;
+    this.endingLocation = endingLocation;
+    
+  }}
+  
+
 
